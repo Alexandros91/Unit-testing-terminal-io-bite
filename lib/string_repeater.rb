53c135c22a478cec931ffefc22f96a8b631ed4c1
Rippling Admin <ripplingadmin@alexanders-mbp.home>
2022-11-24 19:48:28 +0000

    Edge case added, refactoring done

--- lib/string_repeater.rb
+++ lib/string_repeater.rb
@@ -9,9 +9,21 @@ class StringRepeater
     @terminal.puts "Please enter a string"
     string = @terminal.gets.chomp
     @terminal.puts "Please enter a number of repeats"
-    times_repeated = @terminal.gets.to_i
+    times_repeated = enter_number
     @terminal.puts "Here is your result:"
-    @terminal.puts "#{string}" * times_repeated
+    @terminal.puts final_string(string, times_repeated)
+  end
+
+  private
+
+  def enter_number
+    response = @terminal.gets
+    return response.to_i if response.to_i.to_s == response
+    fail "You must enter a number"
+  end
+
+  def final_string(str, num)
+    "#{str}" * num
   end
 
 end
--- spec/string_repeater_spec.rb
+++ spec/string_repeater_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe StringRepeater do
     string_repeater = StringRepeater.new(terminal)
     string_repeater.run
   end
+
+  context 'if we pass a striong instead of a number' do
+    it 'fails' do
+      terminal = double :terminal
+      expect(terminal).to receive(:puts).with("Hello. I will repeat a string many times.").ordered
+      expect(terminal).to receive(:puts).with("Please enter a string").ordered
+      expect(terminal).to receive(:gets).and_return("TWIX").ordered
+      expect(terminal).to receive(:puts).with("Please enter a number of repeats").ordered
+      expect(terminal).to receive(:gets).and_return("Hello").ordered
+      string_repeater = StringRepeater.new(terminal)
+      expect { string_repeater.run }.to raise_error "You must enter a number"
+    end
+  end
 end
